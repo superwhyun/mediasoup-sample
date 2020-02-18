7fcc493557d1ba8e1da0d81d19b1a5ef7c82e95d
SUPERWHYUN <superwhyun@gmail.com>
2020-02-18 17:11:49 +0900

    .

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const app = express();
-const port = 3000;
+const port = 3030;
 const option = {
 	key: fs.readFileSync('../cert/k8s-master.key'),
 	cert: fs.readFileSync('../cert/k8s-master.crt')
@@ -12,7 +12,7 @@ const option = {
 app.use(express.static('public'));
 
 
-https.createServer(option,app).listen(3000, function() {
+https.createServer(option,app).listen(port, function() {
 	console.log(`HTTPS server listening port ${port}`)
 
 });
